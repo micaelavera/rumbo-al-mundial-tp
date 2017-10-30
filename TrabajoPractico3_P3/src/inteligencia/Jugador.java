b1d408ee1104c804fa9b907b013553df625ee306
Micaela <micavera2010@hotmail.com>
2017-10-30 17:02:40 -0300

    Agrego clase Jugador

---
+++ TrabajoPractico3_P3/src/inteligencia/Jugador.java
@@ -0,0 +1,39 @@
+package inteligencia;
+
+public class Jugador {
+
+	private String nombre;
+	private String posicion;
+	private int nivelJuego;
+	private String incompatible;
+	
+	public Jugador(String nombre,String posicion,int nivelJuego,String incompatible){
+		this.nombre=nombre;
+		this.posicion=posicion;
+		this.nivelJuego=nivelJuego;
+		this.incompatible=incompatible;
+	}
+
+	public String nombre() {
+		return nombre;
+	}
+
+	public String posicion() {
+		return posicion;
+	}
+
+	public int nivelJuego() {
+		return nivelJuego;
+	}
+
+	public String incompatible() {
+		return incompatible;
+	}
+	
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + nombre + ", posicion=" + posicion
+				+ ", nivelJuego=" + nivelJuego + ", incompatible="
+				+ incompatible + "]";
+	}
+}

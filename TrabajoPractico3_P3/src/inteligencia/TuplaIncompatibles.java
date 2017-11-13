eeda34cbc4bd798cc97bdec84f25b01bde4b13a9
Liz Katheryna Moreno López <moreno.lizk@gmail.com>
2017-11-13 01:56:36 +0000

    Update TuplaIncompatibles.java

--- TrabajoPractico3_P3/src/inteligencia/TuplaIncompatibles.java
+++ TrabajoPractico3_P3/src/inteligencia/TuplaIncompatibles.java
@@ -13,20 +13,6 @@ public class TuplaIncompatibles<T1, T2>  //Lizz estuvo aqui
 		this.e2 = e2;
 	}
 
-	@Override
-	public boolean equals(Object t2) 
-	{
-		boolean ret = true;
-		if (!(t2 instanceof TuplaIncompatibles))
-			ret = false;
-		else 
-		{
-			// es importante no asumir <T1,T2> para t2,pues podria ser <T3,T4>
-			TuplaIncompatibles<T1, T2> t3 = (TuplaIncompatibles) t2;
-			ret = ret && t3.e1.equals(e1) && t3.e2.equals(e2);
-		}
-		return ret;
-	}
 
 	@Override
 	public String toString() 

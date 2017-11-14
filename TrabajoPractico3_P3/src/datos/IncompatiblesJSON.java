77b499ac88f82cea045b23c82bb440aa7758854d
Micaela <micavera2010@hotmail.com>
2017-11-14 15:48:27 -0300

    Agrego clase Incompatibles

---
+++ TrabajoPractico3_P3/src/datos/IncompatiblesJSON.java
@@ -0,0 +1,63 @@
+package datos;
+
+import inteligencia.Jugador;
+import inteligencia.TuplaIncompatibles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+public class IncompatiblesJSON {
+
+	private ArrayList<TuplaIncompatibles<Jugador, Jugador>> ParesIncompatibles;
+
+	public IncompatiblesJSON(){
+		ParesIncompatibles=new ArrayList<TuplaIncompatibles<Jugador,Jugador>>();
+	}
+	
+	public void agregar(Jugador j1,Jugador j2){
+		ParesIncompatibles.add(new TuplaIncompatibles<Jugador, Jugador>(j1, j2) );
+	}
+
+	public ArrayList<TuplaIncompatibles<Jugador, Jugador>> getParesIncompatibles() {
+		return ParesIncompatibles;
+	}
+	
+	public void generarIncompatiblesGSON(String archivo){
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(this);
+		try{
+			FileWriter writer = new FileWriter(archivo);
+			writer.write(json);
+			writer.close();
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static IncompatiblesJSON leerGSON(String archivo){
+		Gson gson = new Gson();
+		IncompatiblesJSON ret = null;
+		
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(archivo));
+			ret = gson.fromJson(br, IncompatiblesJSON.class);
+		}
+		catch(IOException e){
+			throw new IllegalArgumentException("El sistema no puede encontrar el archivo especificado");
+		}
+		catch(JsonSyntaxException e){
+			throw new IllegalArgumentException("El tipo de archivo no corresponde al formato JSON");
+		}
+		return ret;
+
+	}
+
+}
---
+++ TrabajoPractico3_P3/src/inteligencia/Incompatibles.java
@@ -0,0 +1,86 @@
+package inteligencia;
+
+import inteligencia.PosicionJuego.Posicion;
+
+import java.util.ArrayList;
+
+import datos.IncompatiblesJSON;
+
+public class Incompatibles {
+
+	private ArrayList<TuplaIncompatibles<Jugador, Jugador>> ParesIncompatibles;
+	
+	public Incompatibles(){
+		ParesIncompatibles=new ArrayList<TuplaIncompatibles<Jugador,Jugador>>();
+	}
+	
+	public void agregarPar(Jugador j1, Jugador j2){
+		ParesIncompatibles.add(new TuplaIncompatibles<Jugador, Jugador>(j1, j2) );	
+	}
+	
+	public void cargarJugadores(String archivo){
+		try{
+			IncompatiblesJSON incompatiblesJSON= IncompatiblesJSON.leerGSON(archivo);
+			ParesIncompatibles=incompatiblesJSON.getParesIncompatibles();
+			
+		}catch(IllegalArgumentException e){
+			throw new IllegalArgumentException("No se encuentra el listado de materias con nombre: "+ archivo);
+		}
+	}
+	
+	
+	public void entraUnodelPar(Jugadores jugadores){
+		for(TuplaIncompatibles<Jugador, Jugador> TI: ParesIncompatibles)
+			if ( TI.jugador1.nivelJuego()> TI.jugador2.nivelJuego() ) 
+			{
+				jugadores.eliminarJugador(TI.jugador2);
+			}else{
+				jugadores.eliminarJugador(TI.jugador1);
+			}
+	}
+
+	public ArrayList<TuplaIncompatibles<Jugador, Jugador>> getParesIncompatibles() {
+		return ParesIncompatibles;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		String ret="\nPares de Incompatibles: ";
+		for(TuplaIncompatibles<Jugador, Jugador> TI: ParesIncompatibles){
+			ret+="\n"+TI.jugador1.nombre()+" "+TI.jugador1.nivelJuego()+" "+TI.jugador2.nombre()+" ";
+		}
+		return ret;
+	}
+	
+	public static void main(String[]args){	
+		Jugadores j=new Jugadores();
+		j.agregarJugador(new Jugador("Romero",Posicion.Arquero,10));
+		j.agregarJugador(new Jugador("Marchesin",Posicion.Arquero,5));
+		j.agregarJugador(new Jugador("Otamendi",Posicion.Defensor,6));
+		
+		j.agregarJugador(new Jugador("Mascherano",Posicion.Defensor,5));
+		j.agregarJugador(new Jugador("Fazio",Posicion.Defensor,2));
+		j.agregarJugador(new Jugador("Mercado",Posicion.Defensor,7));
+	
+		System.out.println(j.toString());
+		
+		Incompatibles i=new Incompatibles();
+		i.agregarPar(j.getJugador(1), j.getJugador(2));
+		i.agregarPar(j.getJugador(0), j.getJugador(3));
+	
+		System.out.println(i.toString());
+		i.entraUnodelPar(j);
+		
+		System.out.println(j.toString());
+	}
+
+//	public ArrayList<Jugador> get_jugadores() {
+//		return _jugadores;
+//	}
+	
+	
+
+	
+	
+}

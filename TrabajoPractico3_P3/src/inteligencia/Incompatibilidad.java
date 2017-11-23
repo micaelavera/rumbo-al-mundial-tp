485daaa088174c7b2b4d5387c23a19133a3839f2
Micaela <micavera2010@hotmail.com>
2017-11-23 16:30:28 -0300

    Agrego clase Incompatibilidad

---
+++ TrabajoPractico3_P3/src/inteligencia/Incompatibilidad.java
@@ -0,0 +1,52 @@
+package inteligencia;
+
+import java.util.ArrayList;
+
+import datos.IncompatiblesJSON;
+
+public class Incompatibilidad {
+
+	private ArrayList<TuplaIncompatibles<Jugador, Jugador>> paresIncompatibles;
+	
+	public Incompatibilidad(){
+		paresIncompatibles=new ArrayList<TuplaIncompatibles<Jugador,Jugador>>();
+	}
+	
+	public void agregarPar(Jugador j1, Jugador j2){
+		paresIncompatibles.add(new TuplaIncompatibles<Jugador, Jugador>(j1, j2));	
+	}
+	
+	public void cargarJugadores(String archivo){
+		try{
+			IncompatiblesJSON incompatiblesJSON=IncompatiblesJSON.leerGSON(archivo);
+			paresIncompatibles=incompatiblesJSON.getParesIncompatibles();
+			
+		}catch(IllegalArgumentException e){
+			throw new IllegalArgumentException("No se encuentra el listado de jugadores incompatibles: "+ archivo);
+		}
+	}
+	
+	public void entraUnodelPar(Jugadores jugadores){
+		for(TuplaIncompatibles<Jugador, Jugador> TI: paresIncompatibles)
+			if ( TI.jugador1.nivelJuego()> TI.jugador2.nivelJuego()) {
+				jugadores.eliminarJugador(TI.jugador2);
+			}else{
+				jugadores.eliminarJugador(TI.jugador1);
+			}
+	}
+
+	public ArrayList<TuplaIncompatibles<Jugador, Jugador>> getParesIncompatibles() {
+		return paresIncompatibles;
+	}
+	
+	@Override
+	public String toString(){
+		String ret="\nPares de Incompatibles: ";
+		for(TuplaIncompatibles<Jugador, Jugador> TI: paresIncompatibles){
+			ret+="\n"+TI.jugador1.nombre()+" "+TI.jugador1.nivelJuego()+" "+TI.jugador2.nombre()+" ";
+		}
+		return ret;
+	}
+	
+	
+}
